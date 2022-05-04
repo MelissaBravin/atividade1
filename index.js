75e7b278b8e5538678dea756d5350dff1fece688
Melissa Sa <86063323+MelissaBravin@users.noreply.github.com>
2022-05-04 21:12:39 +0000

    Criei o método por id especifico do put, delete e get.

--- index.js
+++ index.js
@@ -12,9 +12,24 @@ app.get("/", async function(req, res){
   res.json(visualizar);
 });
 
+app.get("/:id", async function(req, res){
+  var mostrar = await usuario.findOne({where: {id: req.params.id}});
+  res.json(mostrar);
+});
+
 app.post("/", async function(req, res){
    var visualizar = await usuario.create(req.body);
-  res.send(visualizar);
+  res.json(visualizar);
+});
+
+app.put("/:id", async function(req, res){
+   var visualizar = await usuario.update(req.body,{where: {id: req.params.id}});
+  res.json(visualizar);
+});
+
+app.delete("/:id", async function(req, res){
+   var visualizar = await usuario.destroy({where: {id: req.params.id}});
+  res.json(visualizar);
 });
 
 app.listen(3000, function(){
